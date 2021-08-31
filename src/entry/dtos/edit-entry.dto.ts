56651bf535681030cbb3042899a3421c0863a22d
cld_rojas <cld.rojasz@gmail.com>
2021-08-31 11:10:58 -0400

    changed default formatter for ts

--- src/entry/dtos/edit-entry.dto.ts
+++ src/entry/dtos/edit-entry.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateEntryDto } from './create-entry.dto';
 
-// eslint-disable-next-line prettier/prettier
-export class EditEntryDto extends PartialType(CreateEntryDto) { }
+export class EditEntryDto extends PartialType(CreateEntryDto) {}
