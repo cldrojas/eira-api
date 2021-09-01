83360eb69e31040b0dce70f053203adc44d42d73
cld_rojas <cld.rojasz@gmail.com>
2021-09-01 13:46:36 -0400

    add Procfile to specify production script on startup

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
   logger.verbose(`Server running on ${await app.getUrl()}`);
 }
 bootstrap();
