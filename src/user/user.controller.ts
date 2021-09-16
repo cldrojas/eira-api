59b78b43eee080fb1806e7ce0748517654d95f44
cld_rojas <cld.rojasz@gmail.com>
2021-09-16 02:40:24 -0300

    update: new endpoint for users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,7 +11,7 @@ import { CreateUserDto, EditUserDto } from './dtos';
 
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
