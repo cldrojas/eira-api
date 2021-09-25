fc1bf51d75bb73389414b33c57295576fb6fe0e5
cld_rojas <cld.rojasz@gmail.com>
2021-09-25 00:43:01 -0300

    add enableCors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
+  app.enableCors();
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
   logger.verbose(`Server running on ${await app.getUrl()}`);
 }
